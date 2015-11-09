5ee113e486001eefb7971f513aea964a00f585b0
ZhuangEr <huangyu.wuhan@gmail.com>
2015-11-09 15:42:53 -0500

    detach evolution operator

--- ga_class.py
+++ ga_class.py
@@ -4,23 +4,32 @@ from random import randint
 import random
 from math import sqrt
 
+vertexs = [(2,3), (3,2), (2,2), (3,3)]
 class GeneticFunction(object):
 	
-	def __init__(self, mutate_rate=0.01,start=(0.0, 0.0), goal=(3.0, 3.0), precise=1):
+	def __init__(self, mutate_rate=0.01,start=(0.0, 0.0), goal=(3.0, 3.0), precise=1, retain_rate=0.2):
 		self.mutate_rate = mutate_rate
+		self.start = start
 		self.goal = goal
 		self.precise = precise
-		graded = [ (fitness(x), x) for x in pop]
-		graded = [ x[1] for x in sorted(graded)]
-		retain_length = int(len(graded)*retain)
-		parents = graded[:retain_length]
+		self.retain_rate = retain_rate
 		
 	
 	
 	def mutation(self, individual):
-		individual[pos_to_mutate] = (round(random.uniform(self.start[0], self.goal[0]),self.precise), 
-									round(random.uniform(self.start[0], self.goal[0]),self.precise))
-		return parents
+		if self.mutate_rate > random.random():
+			pos_to_mutate = randint(0, len(individual)-1)
+			individual[pos_to_mutate] = (round(random.uniform(self.start[0], self.goal[0]),self.precise), 
+										round(random.uniform(self.start[0], self.goal[0]),self.precise))
+		return individual
+		
+	def select(self, population):
+		graded_pop = [ (self.fitness(x), x) for x in population]
+		graded_pop = [ x[1] for x in sorted(graded_pop)]
+		retain_length = int(len(population)*self.retain_rate)
+		retain_pop = graded_pop[:retain_length]
+		return retain_pop
+		
 		
 	def crossover(self, population, crossover_pos=None):
 		population_length = len(population)
@@ -28,15 +37,15 @@ class GeneticFunction(object):
 		female_pos = randint(0, population_length-1)
 		children = []
 		if male_pos != female_pos:
-			male = parents[male_pos]
-			female = parents[female_pos]
+			male = population[male_pos]
+			female = population[female_pos]
 			if crossover_pos == None:
 				crossover_pos = len(male) / 2
 			child = male[:crossover_pos] + female[crossover_pos:]
 			children.append(child)
 
 		population.extend(children)
-		return parents
+		return population
 
 	def fitness(self, individual):
 		"""
@@ -55,15 +64,43 @@ class GeneticFunction(object):
 			previous_point = point
 		
 		sum += sqrt((self.goal[0] - previous_point[0])**2 + (self.goal[1] - previous_point[1])**2)
-		if collision_detect(vertexs, previous_point, goal):
+		if collision_detect(vertexs, previous_point, self.goal):
 			sum += 100
 		return sum
 		
-	def graded(self, population):
-		graded_pop = [ (fitness(x), x) for x in population]
-		graded_pop = [ x[1] for x in sorted(graded)]
-		return graded_pop
-
+	
+def collision_detect(vertexs, point1, point2):
+	positive_flag = 0
+	negative_flag = 0
+	step_b = False
+	line = lambda x1, y1, x2, y2, x, y: (y2-y1)*x + (x1-x2)*y + (x2*y1 - x1*y2)
+	for vertex in vertexs:
+		if line(point1[0], point1[1],point2[0], point2[1], vertex[0], vertex[1]) == 0:
+			step_b = True
+			break
+		elif line(point1[0], point1[1],point2[0], point2[1], vertex[0], vertex[1]) < 0:
+			negative_flag += 1
+		else:
+			positive_flag += 1
+	if step_b or (not step_b and (positive_flag * negative_flag) != 0):
+		max_x = 0.0
+		max_y = 0.0
+		min_x = 100.0 
+		min_y = 100.0
+		for vertex in vertexs:
+			max_x = max(max_x, vertex[0])
+			min_x = min(min_x, vertex[0])
+			max_y = max(max_y, vertex[1])
+			min_y = min(min_y, vertex[1])
+		if (point1[0] > max_x and point2[0] > max_x) or \
+			(point1[0] < min_x and point2[0] < min_x) or \
+			(point1[1] > max_y and point2[1] > max_y) or \
+			(point1[1] < min_y and point2[1] < min_y):
+			return False
+		else:
+			return True
+	else:
+		return False
 
 
 
